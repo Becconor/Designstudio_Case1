839cbb955e4128611e137e23c7fbbe647ac1c8ef
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2025-10-13 13:16:18 +0200

    Fixat hem-knapp
    Fixat så att hemknappen går att använda.

--- index2.js
+++ index2.js
@@ -27,6 +27,27 @@ for (let i = 1; i <= 40; i++) {
 let background1 = document.createElement("div");
 let logoImage = document.createElement("img");
 let background2 = document.createElement("div");
+let homePage = document.createElement("div");
+let buttons = document.createElement("div");
+let button1 = document.createElement("div");
+let button1Image = document.createElement("img");
+let button2 = document.createElement("div");
+let button2Image = document.createElement("img");
+let button3 = document.createElement("div");
+let button3Image = document.createElement("img");
+let button4 = document.createElement("p");
+let txt = "Recipies";
+let infoDiv = document.createElement("div");
+let infoButton = document.createElement("button");
+let getStartedDiv = document.createElement("div");
+let bigText = document.createElement("p");
+let getStartedButton = document.createElement("button");
+let background3 = document.createElement("div");
+let eggText = document.createElement("p");
+let homeButton = document.createElement("button");
+let timer = document.createElement("p");
+let eggTextDiv = document.createElement("div");
+
 
 
 function firstPage() {
@@ -37,16 +58,13 @@ function firstPage() {
     background1.style.backgroundColor = "#FAF8EC";
     background1.style.width = "390px";
     background1.style.height = "764px";
-
     background1.style.display = "flex";
     background1.style.flexDirection = "column";
     background1.style.justifyContent = "center";
     background1.style.alignItems = "center";
-
     background1.style.position = "absolute";
     background1.style.top = "40px";
     background1.style.bottom = "40px";
-
 
 
     logoImage.src = "Images/Logo.png";
@@ -56,7 +74,6 @@ function firstPage() {
     logoImage.style.top = "64px";
 
 
-
     background2.style.backgroundColor = "#FAF8EC";
     background2.style.width = "310px";
     background2.style.height = "424px";
@@ -64,20 +81,17 @@ function firstPage() {
     background2.style.display = "flex";
     background2.style.flexDirection = "column";
     background2.style.alignItems = "center";
-
     background2.style.position = "absolute";
     background2.style.top = "337px";
     background2.style.bottom = "80px";
 
 
-
-    let buttons = document.createElement("div");
+    buttons = document.createElement("div");
     buttons.style.width = "100%";
     buttons.style.display = "flex";
 
 
-
-    let button1 = document.createElement("div");
+    button1 = document.createElement("div");
     button1.style.width = "77.5px";
     button1.style.height = "60px";
     button1.style.borderTop = "1px solid #000000";
@@ -88,15 +102,13 @@ function firstPage() {
     button1.style.justifyContent = "center";
     button1.style.alignItems = "center";
     button1.style.cursor = "pointer";
-
-    let button1Image = document.createElement("img");
+    button1Image = document.createElement("img");
     button1Image.style.width = "60px";
     button1Image.src = "Images/Soft.png";
     button1Image.alt = "Soft egg";
 
 
-
-    let button2 = document.createElement("div");
+    button2 = document.createElement("div");
     button2.style.width = "77.5px";
     button2.style.height = "60px";
     button2.style.border = "1px solid #000000";
@@ -105,15 +117,13 @@ function firstPage() {
     button2.style.justifyContent = "center";
     button2.style.alignItems = "center";
     button2.style.cursor = "pointer";
-
-    let button2Image = document.createElement("img");
+    button2Image = document.createElement("img");
     button2Image.style.width = "60px";
     button2Image.src = "Images/Medium.png";
     button2Image.alt = "Medium egg";
 
 
-
-    let button3 = document.createElement("div");
+    button3 = document.createElement("div");
     button3.style.width = "77.5px";
     button3.style.height = "60px";
     button3.style.border = "1px solid #000000";
@@ -122,15 +132,13 @@ function firstPage() {
     button3.style.justifyContent = "center";
     button3.style.alignItems = "center";
     button3.style.cursor = "pointer";
-
-    let button3Image = document.createElement("img");
+    button3Image = document.createElement("img");
     button3Image.style.width = "60px";
     button3Image.src = "Images/Hard.png";
     button3Image.alt = "Hard egg";
 
 
-
-    let button4 = document.createElement("p");
+    button4 = document.createElement("p");
     button4.style.color = "#EE740A";
     button4.style.fontFamily = "Kaushan";
     button4.style.fontSize = "16px";
@@ -143,25 +151,23 @@ function firstPage() {
     button4.style.justifyContent = "center";
     button4.style.alignItems = "center";
     button4.style.cursor = "pointer";
-
-    let txt = "Recipies";
+    txt = "Recipies";
     button4.innerHTML = `<span class="first-letter">${txt[0]}</span>${txt.slice(1)}`;
 
 
-
-    let homePage = document.createElement("div");
+    homePage = document.createElement("div");
     homePage.style.height = "364px";
     homePage.style.width = "100%";
     homePage.style.borderLeft = "1px solid #000000";
     homePage.style.borderRight = "1px solid #000000";
     homePage.style.borderBottom = "1px solid #000000";
 
-    let infoDiv = document.createElement("div");
+
+    infoDiv = document.createElement("div");
     infoDiv.style.display = "flex";
     infoDiv.style.justifyContent = "flex-end";
     infoDiv.style.padding = "25px 25px";
-
-    let infoButton = document.createElement("button");
+    infoButton = document.createElement("button");
     infoButton.textContent = "i"
     infoButton.style.border = "2px solid #000000";
     infoButton.style.borderRadius = "50%";
@@ -171,8 +177,7 @@ function firstPage() {
     infoButton.style.cursor = "pointer";
 
 
-
-    let getStartedDiv = document.createElement("div");
+    getStartedDiv = document.createElement("div");
     getStartedDiv.style.display = "flex";
     getStartedDiv.style.flexDirection = "column";
     getStartedDiv.style.justifyContent = "center";
@@ -181,12 +186,12 @@ function firstPage() {
     getStartedDiv.style.padding = "0 25px 50px";
     getStartedDiv.style.gap = "50px";
 
-    let softText = document.createElement("p");
-    softText.textContent = "Soft Boiled";
-    softText.style.fontFamily = "Kaushan";
-    softText.style.fontSize = "48px";
+    bigText = document.createElement("p");
+    bigText.textContent = "Soft Boiled";
+    bigText.style.fontFamily = "Kaushan";
+    bigText.style.fontSize = "48px";
 
-    let getStartedButton = document.createElement("button");
+    getStartedButton = document.createElement("button");
     getStartedButton.textContent = "Boil Your Egg";
     getStartedButton.style.fontSize = "36px";
     getStartedButton.style.backgroundColor = "#000000";
@@ -197,9 +202,12 @@ function firstPage() {
     getStartedButton.style.cursor = "pointer";
 
 
-
-    infoButton.addEventListener("click", getInfo);
-
+    infoButton.addEventListener("click", () => getInfo("firstPage"));
+    button1.addEventListener("click", getTimers);
+    /* button2.addEventListener("click", getTimers);
+    button3.addEventListener("click", getTimers);
+    button4.addEventListener("click", getTimers); */
+    getStartedButton.addEventListener("click", getTimers);
 
 
     document.body.append(background1);
@@ -207,7 +215,7 @@ function firstPage() {
     document.body.append(background2);
     homePage.append(infoDiv, getStartedDiv)
     infoDiv.append(infoButton);
-    getStartedDiv.append(softText, getStartedButton);
+    getStartedDiv.append(bigText, getStartedButton);
     button1.append(button1Image);
     button2.append(button2Image);
     button3.append(button3Image);
@@ -223,10 +231,13 @@ firstPage();
 
 
 
-function getInfo() {
+function getInfo(value) {
     background1.remove();
     logoImage.remove();
     background2.remove();
+    background3.remove();
+
+
 
     let backgroundInfo1 = document.createElement("div");
     backgroundInfo1.style.backgroundColor = "#FAF8EC";
@@ -287,59 +298,195 @@ function getInfo() {
     backgroundInfo1.append(xClose, xCloseText);
     xClose.append(xCloseButton);
 
-    xCloseButton.addEventListener('click', () => {
-        backgroundInfo1.remove();
-        firstPage();
-    });
+    if (value === "firstPage") {
+        xCloseButton.addEventListener('click', () => {
+            backgroundInfo1.remove();
+            firstPage();
+        });
+    } else if (value === "getTimers") {
+        xCloseButton.addEventListener('click', () => {
+            backgroundInfo1.remove();
+            getTimers();
+        });
+    }
 };
 
 
+function getTimers() {
+    background1.remove();
+    logoImage.remove();
+    background2.remove();
+
+
+    background3 = document.createElement("div");
+    background3.style.backgroundColor = "#FAF8EC";
+    background3.style.width = "310px";
+    background3.style.height = "720px";
+    background3.style.borderRadius = "20px 20px 0 0";
+    /* background3.style.display = "flex";
+    background3.style.flexDirection = "column";
+    background3.style.justifyContent = "center";
+    background3.style.alignItems = "center"; */
+    background3.style.position = "absolute";
+    background3.style.top = "84px";
+    background3.style.bottom = "40px";
+
+
+    buttons = document.createElement("div");
+    buttons.style.width = "100%";
+    buttons.style.display = "flex";
+
+    button1 = document.createElement("div");
+    button1.style.width = "77.5px";
+    button1.style.height = "60px";
+    button1.style.borderTop = "1px solid #000000";
+    button1.style.borderRight = "1px solid #000000";
+    button1.style.borderLeft = "1px solid #000000";
+    button1.style.borderRadius = "20px 20px 0 0";
+    button1.style.display = "flex";
+    button1.style.justifyContent = "center";
+    button1.style.alignItems = "center";
+    button1.style.cursor = "pointer";
+    button1Image = document.createElement("img");
+    button1Image.style.width = "60px";
+    button1Image.src = "Images/Soft.png";
+    button1Image.alt = "Soft egg";
+
+    button2 = document.createElement("div");
+    button2.style.width = "77.5px";
+    button2.style.height = "60px";
+    button2.style.border = "1px solid #000000";
+    button2.style.borderRadius = "20px 20px 0 0";
+    button2.style.display = "flex";
+    button2.style.justifyContent = "center";
+    button2.style.alignItems = "center";
+    button2.style.cursor = "pointer";
+    button2Image = document.createElement("img");
+    button2Image.style.width = "60px";
+    button2Image.src = "Images/Medium.png";
+    button2Image.alt = "Medium egg";
+
+    button3 = document.createElement("div");
+    button3.style.width = "77.5px";
+    button3.style.height = "60px";
+    button3.style.border = "1px solid #000000";
+    button3.style.borderRadius = "20px 20px 0 0";
+    button3.style.display = "flex";
+    button3.style.justifyContent = "center";
+    button3.style.alignItems = "center";
+    button3.style.cursor = "pointer";
+    button3Image = document.createElement("img");
+    button3Image.style.width = "60px";
+    button3Image.src = "Images/Hard.png";
+    button3Image.alt = "Hard egg";
+
+    button4 = document.createElement("p");
+    button4.style.color = "#EE740A";
+    button4.style.fontFamily = "Kaushan";
+    button4.style.fontSize = "16px";
+    button4.style.width = "77.5px";
+    button4.style.height = "60px";
+    button4.style.border = "1px solid #000000";
+    button4.style.borderRadius = "20px 20px 0 0";
+    button4.textContent = "Recipes";
+    button4.style.display = "flex";
+    button4.style.justifyContent = "center";
+    button4.style.alignItems = "center";
+    button4.style.cursor = "pointer";
+    txt = "Recipies";
+    button4.innerHTML = `<span class="first-letter">${txt[0]}</span>${txt.slice(1)}`;
 
 
 
 
+    homePage = document.createElement("div");
+    homePage.style.height = "660px";
+    homePage.style.borderLeft = "1px solid #000000";
+    homePage.style.borderRight = "1px solid #000000";
+    homePage.style.borderBottom = "1px solid #000000";
+
+    infoDiv = document.createElement("div");
+    infoDiv.style.display = "flex";
+    infoDiv.style.justifyContent = "flex-end";
+    infoDiv.style.padding = "25px 25px";
+
+    infoButton = document.createElement("button");
+    infoButton.textContent = "i"
+    infoButton.style.border = "2px solid #000000";
+    infoButton.style.borderRadius = "50%";
+    infoButton.style.width = "24px";
+    infoButton.style.height = "24px";
+    infoButton.style.backgroundColor = "#FAF8EC";
+    infoButton.style.cursor = "pointer";
+
+    getStartedDiv = document.createElement("div");
+    getStartedDiv.style.display = "flex";
+    getStartedDiv.style.flexDirection = "column";
+    getStartedDiv.style.justifyContent = "space-between";
+    getStartedDiv.style.alignItems = "center";
+    getStartedDiv.style.height = "585px";
+    getStartedDiv.style.padding = "0 25px 50px";
+    getStartedDiv.style.gap = "50px";
+
+    eggTextDiv = document.createElement("div");
+    eggTextDiv.style.textAlign = "center";
+    eggTextDiv.style.display = "flex";
+    eggTextDiv.style.flexDirection = "column";
+
+
+    bigText = document.createElement("p");
+    bigText.textContent = "Soft Boiled";
+    bigText.style.fontFamily = "Kaushan";
+    bigText.style.fontSize = "48px";
+
+    eggText = document.createElement("p");
+    eggText.textContent = "Egg";
+    eggText.style.color = "#EE740A";
+    eggText.style.fontSize = "36px"
+
+
+    timer = document.createElement("p");
+    timer.textContent = "07:00";
+    timer.style.fontSize = "80px"
+
+    getStartedButton = document.createElement("button");
+    getStartedButton.textContent = "START";
+    getStartedButton.style.fontSize = "36px";
+    getStartedButton.style.backgroundColor = "#000000";
+    getStartedButton.style.color = "#EE740A";
+    getStartedButton.style.border = "0";
+    getStartedButton.style.borderRadius = "20px";
+    getStartedButton.style.padding = "10px 20px";
+    getStartedButton.style.cursor = "pointer";
+
+    homeButton = document.createElement("button");
+    homeButton.textContent = "Home";
+    homeButton.style.fontSize = "24px";
+    homeButton.style.fontFamily = "Kaushan";
+    homeButton.style.textDecoration = "underline";
+    homeButton.style.background = "#FAF8EC";
+    homeButton.style.border = "0";
+    homeButton.style.cursor = "pointer";
+
+
+    infoButton.addEventListener("click", () => getInfo("getTimers"));
+    homeButton.addEventListener("click", () => {
+        background3.remove();
+        firstPage()
+    });
+
+
+    document.body.append(background3);
+    button1.append(button1Image);
+    button2.append(button2Image);
+    button3.append(button3Image);
+    buttons.append(button1, button2, button3, button4);
+    homePage.append(infoDiv, getStartedDiv)
+    infoDiv.append(infoButton);
+    eggTextDiv.append(bigText, eggText);
+    getStartedDiv.append(eggTextDiv, timer, getStartedButton, homeButton);
+    background3.append(buttons, homePage);
+}
 
 
 
-
-
-
-let background3 = document.createElement("div");
-background3.style.backgroundColor = "#FAF8EC";
-background3.style.width = "310px";
-background3.style.height = "720px";
-background3.style.borderRadius = "20px 20px 0 0";
-
-background3.style.display = "flex";
-background3.style.flexDirection = "column";
-background3.style.justifyContent = "center";
-background3.style.alignItems = "center";
-
-background3.style.position = "absolute";
-background3.style.top = "84px";
-background3.style.bottom = "40px";
-
-/* document.body.append(background3); */
-
-
-
-
-
-let backgroundInfo2 = document.createElement("div");
-backgroundInfo2.style.backgroundColor = "#FAF8EC";
-backgroundInfo2.style.width = "310px";
-backgroundInfo2.style.height = "720px";
-
-backgroundInfo2.style.border = "1px solid #000000"
-backgroundInfo2.style.borderRadius = "20px 20px 0 0";
-
-backgroundInfo2.style.display = "flex";
-backgroundInfo2.style.flexDirection = "column";
-backgroundInfo2.style.justifyContent = "center";
-backgroundInfo2.style.alignItems = "center";
-
-backgroundInfo2.style.position = "absolute";
-backgroundInfo2.style.top = "84px";
-backgroundInfo2.style.bottom = "40px";
-
-/* document.body.append(backgroundInfo2); */
